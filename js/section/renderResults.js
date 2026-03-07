859158723bbdfe320ca9fc8dd185f86211fbae56
Sarash Basumatary <58624611+sarashbsty@users.noreply.github.com>
2026-03-08 01:51:31 +0530

    Update renderResults.js
    used box to enclose expressions

--- js/section/renderResults.js
+++ js/section/renderResults.js
@@ -6,8 +6,23 @@ export default function renderResults(data)
   h2.textContent = "Final Minimized Expression";
 
   const result = document.createElement('div');
-  result.className = "mono text-box";
-  result.textContent = 'F = ' + data.join(' + ');
+  result.className = "flex";
+  result.style.alignItems = 'center';
+  result.style.fontSize = '20px';
+  
+  const arr = ['F = '];
+  arr.push(data[0]);
+  data.slice(1).forEach(exp => { arr.push(' + '); arr.push(exp); });
+  
+  arr.forEach((elem,i) => {
+	  const expression = document.createElement('span');
+	  expression.textContent = elem;
+	  
+	  if((i+1)%2 == 0) expression.className = "mono box";
+	  else expression.className = "mono";
+	  
+	  result.append(expression);
+  });
 
   section.append(h2,result);
   return section;
